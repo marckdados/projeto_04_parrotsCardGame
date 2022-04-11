24030180992b8726760bb52af9809d13066dbcf3
Bismarck <bismarckdados@gmail.com>
2022-04-11 15:27:58 -0300

    Colocando duas cartas iguais e refatorando o código.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,38 @@
-const cartasAnimadas = ['/images/bobrossparrot.gif','/images/explodyparrot.gif','/images/fiestaparrot.gif','/images/metalparrot.gif','/images/revertitparrot.gif','/images/tripletsparrot.gif','/images/unicornparrot.gif']
-
+const cartasAnimadas = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot', 'unicornparrot'];
+cartasAnimadas.sort(comparador);
+let elemento1;
+let elemento2;
+let total = 0;
 
 function colocarCartas(){
     let numeroDeCartas  = prompt('Quantas cartas ?');
-    let list = document.querySelector('.cartas');
     numeroDeCartas = Number(numeroDeCartas);
     if(numeroDeCartas >= 4 && numeroDeCartas <= 12 && numeroDeCartas%2 === 0 && typeof numeroDeCartas == "number" ){
-        for(let i = 0; i < numeroDeCartas;i++){
-            list.innerHTML += `<li class='carta' onclick='selecionarCarta(this)'><img class='front' src='/images/front.png'><img class='gif escondido' src='${cartasAnimadas[aleatorio(6)]}'></li>`;
-        }
+        criaCartas(numeroDeCartas);
     }else{
         alert('Erro');
         colocarCartas();
     }  
+}
+
+function criaCartas(numeroDeCartas){
+    const repositorio = [];
+    repositorio.sort(comparador);
+     cartas = document.querySelector('.cartas');
+     cartas.innerHTML = ""
+     
+    total += numeroDeCartas
+    for(let i = 0; i < numeroDeCartas / 2; i++){
+        repositorio.push(cartasAnimadas[i]);
+        repositorio.push(cartasAnimadas[i]);
+    }
+    repositorio.sort(comparador);
+    
+    for (let j = 0; j < numeroDeCartas; j++){
+        cartas.innerHTML += `
+        <li class='carta' onclick='selecionarCarta(this)'><img class='front' src='/images/front.png'><img class='gif escondido' src='images/${repositorio[j]}.gif'></li>`;
+    }
+      
 }
 
 function selecionarCarta(elemento){
@@ -23,6 +43,10 @@ function selecionarCarta(elemento){
 function aleatorio(max){
     
     return Math.ceil(Math.random() * (max - 0) + 0);
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
 colocarCartas();
