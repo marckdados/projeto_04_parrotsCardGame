f8e616b59034c18dd3958f3fba48a47850721a21
Bismarck <bismarckdados@gmail.com>
2022-04-11 16:52:26 -0300

    Adicionando rotação e travamento da carta até selecionar outra.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,10 @@ const cartasAnimadas = ['bobrossparrot','explodyparrot','fiestaparrot','metalpar
 cartasAnimadas.sort(comparador);
 let elemento1;
 let elemento2;
+let cartas;
+let contador = 0;
 let total = 0;
+let par =0;
 
 function colocarCartas(){
     let numeroDeCartas  = prompt('Quantas cartas ?');
@@ -16,28 +19,63 @@ function colocarCartas(){
 }
 
 function criaCartas(numeroDeCartas){
-    const repositorio = [];
-    repositorio.sort(comparador);
+    const guardarCartas = [];
+    guardarCartas.sort(comparador);
      cartas = document.querySelector('.cartas');
      cartas.innerHTML = ""
      
     total += numeroDeCartas
     for(let i = 0; i < numeroDeCartas / 2; i++){
-        repositorio.push(cartasAnimadas[i]);
-        repositorio.push(cartasAnimadas[i]);
+        guardarCartas.push(cartasAnimadas[i]);
+        guardarCartas.push(cartasAnimadas[i]);
     }
-    repositorio.sort(comparador);
+    guardarCartas.sort(comparador);
     
     for (let j = 0; j < numeroDeCartas; j++){
         cartas.innerHTML += `
-        <li class='carta' onclick='selecionarCarta(this)'><img class='front' src='/images/front.png'><img class='gif escondido' src='images/${repositorio[j]}.gif'></li>`;
+        <li class='carta' onclick='selecionarCarta(this)'>
+        <div class='front face' >
+        <img src='/images/front.png'>
+        </div>
+        <div class='back face'>
+        <img  src='images/${guardarCartas[j]}.gif'></li>
+        </div>
+        `;
+        
     }
       
 }
 
 function selecionarCarta(elemento){
-    elemento.querySelector('.front').classList.add('escondido');
-    elemento.querySelector('.gif').classList.remove('escondido');
+    if (elemento.classList.contains("rotate")){
+        return; 
+    }
+    elemento.classList.add("rotate");
+    contador++;
+
+    const primeira = elemento1 == undefined;
+    if (primeira){
+        elemento1 = elemento;
+        return;
+    }
+
+    elemento2 = elemento;
+
+   
+    if (elemento1.innerHTML == elemento2.innerHTML){
+        par++;
+    }
+
+    const errado = elemento1.innerHTML != elemento2.innerHTML;
+
+   if (errado){
+       setTimeout(retornaOrigem, 1000, elemento1, elemento2);
+   }
+
+    elemento1 = undefined;
+   elemento2 = undefined;
+   venceu()
+
 }
 
 function aleatorio(max){
@@ -50,3 +88,8 @@ function comparador() {
 }
 
 colocarCartas();
+
+function retornaOrigem(elemento1, elemento2){
+    elemento1.classList.remove("rotate");
+    elemento2.classList.remove("rotate");
+}
